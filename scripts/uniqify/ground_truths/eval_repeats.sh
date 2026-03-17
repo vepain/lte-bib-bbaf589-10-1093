f9b79da6bbc88a9fda1ade5446cc9e251c2c7adc
vepain <victorepain@disroot.org>
2026-03-16 18:33:12 -0700

    fix: :bug: Use correct samples list

--- scripts/uniqify/ground_truths/eval_repeats.sh
+++ scripts/uniqify/ground_truths/eval_repeats.sh
@@ -4,5 +4,5 @@ contents="only_plasmids with_chromosomes"
 
 for content in $contents; do
     gt_dir=ground_truths/$content
-    uv run lteu eval run "$gt_dir" "$gt_dir" samples/complete_hybrid_asm.tsv "comp_hom/$content/repeats/ground_truths.tsv"
+    uv run lteu eval run "$gt_dir" "$gt_dir" "samples/repeats/$content.tsv" "comp_hom/$content/repeats/ground_truths.tsv"
 done
--- scripts/uniqify/ground_truths/eval_uniqify.sh
+++ scripts/uniqify/ground_truths/eval_uniqify.sh
@@ -4,5 +4,5 @@ contents="only_plasmids with_chromosomes"
 
 for content in $contents; do
     gt_dir=uniqify/$content/ground_truths
-    uv run lteu eval run "$gt_dir" "$gt_dir" samples/complete_hybrid_asm.tsv "comp_hom/$content/uniqify/ground_truth.tsv"
+    uv run lteu eval run "$gt_dir" "$gt_dir" "samples/repeats/$content.tsv" "comp_hom/$content/uniqify/ground_truth.tsv"
 done
